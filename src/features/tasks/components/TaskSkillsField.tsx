d0a9c00526f6622bdfbbde88a87ad97604db2917
Sze Ying <bloom9334@hotmail.com>
2025-10-13 23:26:34 +0800

    refactor: move SkillPill into another folder

--- src/features/tasks/components/TaskSkillsField.tsx
+++ src/features/tasks/components/TaskSkillsField.tsx
@@ -1,6 +1,6 @@
 import type { TaskFieldPath, TaskFormSectionForm } from './TaskFormSection.types'
 import type { Skill } from '../../../types/tasks'
-import { SkillPill } from './SkillPill'
+import { SkillPill } from '../../skills/components/SkillPill'
 import { TaskFieldMessages } from './TaskFieldMessages'
 
 type TaskSkillsFieldProps = {
