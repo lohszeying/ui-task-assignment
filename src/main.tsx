fd8a36591ee08d9d9e1911758510d893c73e940d
Sze Ying <bloom9334@hotmail.com>
2025-10-13 21:49:00 +0800

    chore: remove unused code

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './index.css'
 import { RouterProvider } from '@tanstack/react-router'
 import { router } from './router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import './index.css'
 
 const queryClient = new QueryClient()
-
-
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
